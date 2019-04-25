823548b9a89d35b8bf76fa81d2ed7fd235e656d6
EsraaMohd <43968540+EsraaMohd@users.noreply.github.com>
2019-04-25 14:07:00 +0300

    open

---
+++ assest/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("ul.dropdown li").hover(function () {
+        $(this).find(".subList").slideDown(300);
+    }, function () {
+        $(this).find(".subList").slideUp(300);
+    });
+
+    $("ul.dropdown li").hover(function () {
+        $(this).find(".subForm").slideDown(300);
+    }, function () {
+        $(this).find(".subForm").slideUp(300);
+    });
+});
